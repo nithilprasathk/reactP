b8f5bdecd01089c227ad41a6f74fd76c982dc7b3
nithilprasathk <nithilprasath95@gmail.com>
2024-02-08 18:27:43 +0530

    practice

---
+++ src/components/UserInput.jsx
@@ -0,0 +1,43 @@
+export default function UserInput({onChange,UserInput}) {
+    
+
+    
+    return (
+        <section id="user-input">
+            <div className="input-group">
+                <p>
+                    <label>Initial Investment</label>
+                    <input type="number" required
+                        value={UserInput.initialInvestment}
+                        onChange={(event)=>onChange('initialInvestment',event.target.value)}
+
+                    />
+                    
+                </p>
+                <p>
+                    <label>Annual Investment</label>
+                    <input type="number" required
+                     value={UserInput.annualInvestment}
+                     onChange={(event)=>onChange('annualInvestment',event.target.value)}
+                    />
+                </p>
+            </div>
+            <div className="input-group">
+                <p>
+                    <label>Expected Return</label>
+                    <input type="number" required
+                     value={UserInput.expectedReturn}
+                     onChange={(event)=>onChange('expectedReturn',event.target.value)}
+                    />
+                </p>
+                <p>
+                    <label>Duration</label>
+                    <input type="number" required
+                     value={UserInput.duration}
+                     onChange={(event)=>onChange('duration',event.target.value)}
+                    />
+                </p>
+            </div>
+        </section>
+    )
+}
